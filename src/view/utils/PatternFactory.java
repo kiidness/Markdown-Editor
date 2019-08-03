5804d39ca3bc95be0be9caf39016a5fe3c7767dc
Eliott <eliott.chaput@outlook.fr>
2019-08-03 20:07:25 +0200

    Removing useless comment & finished fixing multilined hightlight

--- src/view/utils/PatternFactory.java
+++ src/view/utils/PatternFactory.java
@@ -4,42 +4,14 @@ public abstract class PatternFactory {
     public static String generateMultilinedBalisePattern (String baliseRegex, String groupName) {
         StringBuilder stringBuilder = new StringBuilder();
 
-
         stringBuilder.append("([^\\\\])(?<");
         stringBuilder.append(groupName);
         stringBuilder.append(">");
         stringBuilder.append(baliseRegex);
-        stringBuilder.append("[^\n]+[^\\\\]");
+        stringBuilder.append("([\n]|.)+[^\\\\]");
         stringBuilder.append(baliseRegex);
         stringBuilder.append(")");
-        /*
 
-        stringBuilder.append("|(?<");
-        stringBuilder.append(groupName);
-        stringBuilder.append(">^(");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append(")[^\n]+[^\\\\](");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append("))");
-        */
-        /*
-        stringBuilder.append("|(?<");
-        stringBuilder.append(groupName);
-        stringBuilder.append(">^(");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append(")([^\n]+[^\\\\](");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append(")$))|");
-
-
-        stringBuilder.append("[^\\\\\\\\](?<");
-        stringBuilder.append(groupName);
-        stringBuilder.append(">((");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append(")[^\n]+[^\\\\](");
-        stringBuilder.append(baliseRegex);
-        stringBuilder.append(")))");
-        */
         return stringBuilder.toString();
     }
 }
